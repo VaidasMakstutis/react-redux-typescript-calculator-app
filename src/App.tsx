822042171a443617102168143ab42d02884c728d
Vaidas <vaidas.makstutis@gmail.com>
2022-08-09 16:27:51 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="App text-center">
+      <h2>This is calculator app!</h2>
+    </div>
+  );
+};
+
+export default App;
